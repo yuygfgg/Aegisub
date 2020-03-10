53cc316e6f740a7d810f3b2b47990b5ff90f5aa0
wangqr <wangqr@wangqr.tk>
2020-03-10 01:15:58 -0400

    Expand layer SpinCtrl a little
    to compensate wxMac bug with GetSizeFromText

--- src/subs_edit_box.cpp
+++ src/subs_edit_box.cpp
@@ -157,7 +157,7 @@ SubsEditBox::SubsEditBox(wxWindow *parent, agi::Context *context)
 	// GTK3 has a bug that we cannot shrink the size of a widget, so do nothing there. See:
 	//  http://gtk.10911.n7.nabble.com/gtk-widget-set-size-request-stopped-working-with-GTK3-td26274.html
 	//  https://trac.wxwidgets.org/ticket/18568
-	layer->SetInitialSize(layer->GetSizeFromTextSize(GetTextExtent(wxS("0"))));
+	layer->SetInitialSize(layer->GetSizeFromTextSize(GetTextExtent(wxS("00"))));
 #endif
 	layer->SetToolTip(_("Layer number"));
 	middle_left_sizer->Add(layer, wxSizerFlags().Center());
