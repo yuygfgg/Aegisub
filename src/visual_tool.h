b1a9867f16755ce7d8bf47b994ac0b3b5810ec34
arch1t3cht <arch1t3cht@gmail.com>
2022-11-04 00:45:32 +0100

    Visual perspective tool: First functioning version

--- src/visual_tool.h
+++ src/visual_tool.h
@@ -127,6 +127,14 @@ protected:
 	void GetLineRotation(AssDialogue *diag, float &rx, float &ry, float &rz);
 	void GetLineShear(AssDialogue *diag, float& fax, float& fay);
 	void GetLineScale(AssDialogue *diag, Vector2D &scale);
+	float GetLineFontSize(AssDialogue *diag);
+	int GetLineAlignment(AssDialogue *diag);
+	/// @brief Compute text extents of the given line without any formatting
+	///
+	/// Formatting tags are stripped and \fs tags are respected, but \fscx and \fscy are kept as 100 even if
+	/// they are different in the style.
+	/// Returns a rough estimate when getting the precise extents fails
+	void GetLineBaseExtents(AssDialogue *diag, double &width, double &height, double &descent, double &extlead);
 	void GetLineClip(AssDialogue *diag, Vector2D &p1, Vector2D &p2, bool &inverse);
 	std::string GetLineVectorClip(AssDialogue *diag, int &scale, bool &inverse);
 
