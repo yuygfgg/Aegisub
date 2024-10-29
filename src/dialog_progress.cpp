de0de6ae5a9f6d9d422ff804cc3e346573efe9cf
arch1t3cht <arch1t3cht@gmail.com>
2024-10-29 14:53:27 +0100

    Revert "Wrap long status messages in progress dialog"
    This reverts commit 88cc98df9c3f12705f52a4046836366e9972dcce.

--- src/dialog_progress.cpp
+++ src/dialog_progress.cpp
@@ -79,15 +79,11 @@ public:
 	DialogProgressSink(DialogProgress *dialog) : dialog(dialog) { }
 
 	void SetTitle(std::string const& title) override {
-		Main().Async([=]{ dialog->title->SetLabelText(to_wx(title)); dialog->Layout(); });
+		Main().Async([=]{ dialog->title->SetLabelText(to_wx(title)); });
 	}
 
 	void SetMessage(std::string const& msg) override {
-		Main().Async([=]{
-			dialog->text->SetLabelText(to_wx(msg));
-			dialog->text->Wrap(dialog->GetMinWidth());
-			dialog->Fit();
-		});
+		Main().Async([=]{ dialog->text->SetLabelText(to_wx(msg)); });
 	}
 
 	void SetProgress(int64_t cur, int64_t max) override {
@@ -121,7 +117,7 @@ DialogProgress::DialogProgress(wxWindow *parent, wxString const& title_text, wxS
 {
 	title = new wxStaticText(this, -1, title_text, wxDefaultPosition, wxDefaultSize, wxALIGN_CENTRE | wxST_NO_AUTORESIZE);
 	gauge = new wxGauge(this, -1, 300, wxDefaultPosition, wxSize(300,20));
-	text = new wxStaticText(this, -1, message, wxDefaultPosition, wxDefaultSize, wxALIGN_CENTRE);
+	text = new wxStaticText(this, -1, message, wxDefaultPosition, wxDefaultSize, wxALIGN_CENTRE | wxST_NO_AUTORESIZE);
 	cancel_button = new wxButton(this, wxID_CANCEL);
 	log_output = new wxTextCtrl(this, -1, "", wxDefaultPosition, wxSize(600, 240), wxTE_MULTILINE | wxTE_READONLY);
 
